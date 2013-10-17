87c6fbd82218bbfb60c0e1f33574ba4da4739087
Anita Jaszewska <anita.jaszewska@wimdu.com>
2013-10-17 16:47:02 +0200

    expressions to solve later

---
+++ ex37.rb
@@ -0,0 +1,98 @@
+KEYWORDS:
+
+  alias
+  and
+  BEGIN
+  begin
+  break
+  case
+  class
+  def
+  defined?
+  do
+  else
+  elsif
+  END
+  end
+  ensure
+  false
+  for
+  if
+  in
+  module
+  next
+  nil
+  not
+  or
+  redo
+  rescue
+  retry
+  return
+  self
+  super
+  then
+  true
+  undef
+  unless
+  until
+  when
+  while
+  yield
+
+DATA TYPES:
+
+  true
+  false
+  nil
+  constants
+  strings
+  numbers
+  ranges
+  arrays
+  hashes
+
+STRING ESCAPE SEQUENCES:
+
+  \\
+  \'
+  \"
+  \a
+  \b
+  \f
+  \n
+  \r
+  \t
+  \v
+
+OPERATORS:
+
+  ::
+  []
+  **
+  -(unary)
+  +(unary)
+  !
+  ~
+  *
+  /
+  %
+  +
+  -
+  <<
+  >>
+  &
+  |
+  >
+  >=
+  <
+  <=
+  <=>
+  ==
+  ===
+  !=
+  =~
+  !~
+  &&
+  ||
+  ..
+  ...
